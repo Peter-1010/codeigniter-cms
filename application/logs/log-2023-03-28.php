1f62a5afa7ef4fbb9ab0fa534af1851f043864ee
El Abasery <peteranwer999@gmail.com>
2023-04-26 07:16:59 +0200

    first commit

---
+++ application/logs/log-2023-03-28.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2023-03-28 15:10:05 --> Severity: Notice  --> Only variables should be assigned by reference C:\wamp64\www\ci_project\system\core\Loader.php 150
